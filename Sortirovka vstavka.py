7547cda7b7ce9758ec900acd4819ce9109da7d1d
Vsevolod <v.ogorodnikov1@shool.nsu.ru>
2024-10-23 13:57:43 +0700

    Сделал сортировку вставкой

---
+++ Sortirovka vstavka.py
@@ -0,0 +1,12 @@
+def insertion_sort(a):
+    for i in range(1, len(a)):
+        tmp = a[i]
+        j=i-1
+        while j >=0 and a[j]>tmp:
+            a[j+1]=a[j]
+            j-=1
+            a[j+1]=tmp
+# проверим
+a = [int(x) for x in input().split()]
+insertion_sort(a)
+print(a)
